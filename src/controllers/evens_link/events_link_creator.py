238759fbcaf8f2c6ce97708a3e394b0ae194d578
Yago Russo <yagrrusso@gmail.com>
2025-02-22 17:06:35 -0300

    feat: last controllers

---
+++ src/controllers/evens_link/events_link_creator.py
@@ -0,0 +1,39 @@
+from src.model.repositories.interfaces.events_link_repository import EventosLinkRepositoryInterface
+
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
+
+class EventsLinkCreator:
+    def __init__(self, events_link_repo: EventosLinkRepositoryInterface):
+        self.__events_link_repo = events_link_repo
+
+    def create(self, http_request: HttpRequest) -> HttpResponse:
+        body = http_request.body["data"]
+
+        self.__check_event_link(body['event_id'], body(['subscriber_id']))
+        new_link = self.__create_event_link(body['event_id'], body(['subscriber_id']))
+        return self.__format_response(new_link, body['event_id'], body(['subscriber_id']))
+
+    def __check_event_link(self, event_id: int, subscriber_id: int) -> None:
+        response = self.__events_link_repo.select(event_id, subscriber_id)
+        if response: raise Exception("Link Already Exists!")
+
+    def __create_event_link(self, event_id: int, subscriber_id: int) -> str:
+        new_link = self.__events_link_repo.insert(event_id, subscriber_id)
+        return new_link
+
+    def __format_response(self, new_link: str, event_id: int, subscriber_id: int) -> HttpResponse:
+        return HttpResponse(
+            body={
+                "data": {
+                    "Type": "Event Link",
+                    "count": 1,
+                    "attributes": {
+                        "link": new_link,
+                        "event_id": event_id,
+                        "subscriber_id": subscriber_id
+                    }
+                }
+            },
+            status_code=201
+        )
---
+++ src/controllers/subscribers/subscribers_manager.py
@@ -0,0 +1,64 @@
+from src.model.repositories.interfaces.subscribers_repository import SubscribersRepositoryInterface
+
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
+
+class SubscriberManager:
+    def __init__(self, subscribers_repo: SubscribersRepositoryInterface):
+        self.__subscribers_repo = subscribers_repo
+
+    def get_subscribers_by_link(self, http_request: HttpRequest) -> HttpResponse:
+        params = http_request.param
+
+        subscribers = self.__subscribers_repo.select_by_link(params['link'], params['event_id'])
+        return self.__format_subs_by_link(subscribers)
+
+    def get_event_ranking(self, http_request: HttpRequest) -> HttpResponse:
+        params = http_request.param
+
+        event_ranking = self.__subscribers_repo.get_ranking(params['event_id'])
+        return self.__format_event_ranking(event_ranking)
+
+    def __format_subs_by_link(self, subscribers: list) -> HttpResponse:
+        formatted_subscriber = []
+
+        for sub in subscribers:
+            formatted_subscriber.append(
+                {
+                    "nome": sub.nome,
+                    "email": sub.email,
+                }
+            )
+            
+        return HttpResponse(
+            body={
+                "data": {
+                    "Type": "Subscriber",
+                    "count": len(formatted_subscriber),
+                    "subscribers": formatted_subscriber
+                }
+            },
+            status_code=200
+        )
+
+    def __format_event_ranking(self, event_ranking: list) -> HttpResponse:
+        formatted_event_ranking = []
+
+        for position in event_ranking:
+            formatted_event_ranking.append(
+                {
+                    "link": position.link,
+                    "total_subscribers": position.total,
+                }
+            )
+            
+        return HttpResponse(
+            body={
+                "data": {
+                    "Type": "Ranking",
+                    "count": len(formatted_event_ranking),
+                    "ranking": formatted_event_ranking
+                }
+            },
+            status_code=200
+        )
